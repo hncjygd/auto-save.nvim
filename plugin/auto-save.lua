8cdfa3b69a8f89e8fb2f715a8ebb3834a46a0a42
XXiaoA <isxxiaoa@gmail.com>
2022-08-07 17:07:53 +0800

    style(stylua): format

--- plugin/auto-save.lua
+++ plugin/auto-save.lua
@@ -1,10 +1,10 @@
 if vim.g.loaded_auto_save then
-  return
+    return
 end
 vim.g.loaded_auto_save = true
 
 local command = vim.api.nvim_create_user_command
 
 command("ASToggle", function()
-	require("auto-save").toggle()
+    require("auto-save").toggle()
 end, {})
